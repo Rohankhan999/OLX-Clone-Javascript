bc42e1b80725b7165a99942804d8f30215edecc0
Rohankhan999 <rohankhan08453@gmail.com>
2024-11-27 23:50:44 +0500

    .

--- app.js
+++ app.js
@@ -3,4 +3,13 @@
     rollNo : 12890,
     Class : "Web App",
 };
-console.log(cardsInformation);
+function card (Model , Price , Colors , Waterproof ){
+    this.name = Model,
+    this.Price = Price,
+    this.Colors = Colors,
+    this.type = Waterproof
+}
+var student1 = new card("Apple 12 pro Max","250000","Red , Green , Purple ","true")
+document.getElementById('object').innerHTML = "<br>"+  "<b>Model Name : </b>" + "  " + student1.name + "<br>" + "<br>" +"<b>Available Colors : </b>"+ student1.Colors + "<br>" + "<br>"  +"<b>Price : </b>"+ student1.Price
+
+
