fc9e55a1e44fa222c89f6b16d6fa9bc8880ad8b0
Rohankhan999 <rohankhan08453@gmail.com>
2024-11-28 12:01:42 +0500

    .

--- app.js
+++ app.js
@@ -3,13 +3,12 @@
     rollNo : 12890,
     Class : "Web App",
 };
-function card (Model , Price , Colors , Waterproof ){
+function card (Model , Price , Colors ,){
     this.name = Model,
     this.Price = Price,
-    this.Colors = Colors,
-    this.type = Waterproof
+    this.Colors = Colors
 }
-var student1 = new card("Apple 12 pro Max","250000","Red , Green , Purple ","true")
-document.getElementById('object').innerHTML = "<br>"+  "<b>Model Name : </b>" + "  " + student1.name + "<br>" + "<br>" +"<b>Available Colors : </b>"+ student1.Colors + "<br>" + "<br>"  +"<b>Price : </b>"+ student1.Price
+var student1 = new card("Apple 12 pro Max","200000","Red , Green , Purple ")
+document.getElementById('object').innerHTML = "<br>"+  "<u><b>Model Name</u> : </b>" + "  " + student1.name + "<br>" + "<br>" +"<u><b>Available Colors</u> : </b>"+ student1.Colors + "<br>" + "<br>"  +"<u> <b>Price</u> : </b>"+ student1.Price;
 
 
